dad5f47cfa54891244ae215c4ced38a51fe5b0a8
Dan Kissell <dkissell@gmail.com>
2011-08-18 12:27:37 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kissell::Application.config.secret_token = '196956611551ed664e095826bf88604239a3dc865c00a23aca04836ec90c73655bc46c4794cfa619bce7e1ceb85d7fa770a187143506b1dd485a9afe7e11dd09'
